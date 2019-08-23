e9ff228623504a69f89f29f6a9cc614d48cb73b7
Josep <apuratepp@gmail.com>
2019-08-23 23:02:42 +0200

    Make the tests pass

--- test/lh_web/controllers/page_controller_test.exs
+++ test/lh_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule LhWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Lh"
   end
 end
